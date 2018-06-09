6039fcbe7b3a00fa68c26a04abfa0801368b733f
lamkakyun <lamkakyun@gmail.com>
2018-06-09 12:56:51 +0800

    no comment

--- tp/application/index/controller/MoneyTrack.php
+++ tp/application/index/controller/MoneyTrack.php
@@ -1,12 +1,13 @@
 <?php
 namespace app\index\controller;
 
+use app\index\service\MoneyService;
 use think\Controller;
 
 class MoneyTrack extends Controller
 {
     /**
-     * add money track
+     * add money track, UI just like calculator
      * @AUTHOR: Lamkakyun
      * @DATE: 2018-05-31 10:44:27
      */
@@ -18,11 +19,29 @@ class MoneyTrack extends Controller
             return $this->fetch();
         }
 
-
+        return MoneyService::getInstance()->addMoneyLog($params);
     }
 
+    /**
+     * show add money items, like food, shopping, bus and so on .
+     * @AUTHOR: Lamkakyun
+     * @DATE: 2018-06-09 12:48:18
+     * @DESCRIPTION:
+     */
     public function moneyItems()
     {
+        $params = request()->request();
+        $fixed_items = [
+            'FOOD',
+            'TRAFFIC',
+            'CLOTH'
+        ];
 
+        if (request()->method() == 'GET')
+        {
+            return $this->fetch();
+        }
+
+        return MoneyService::getInstance()->addMoneyItem($params);
     }
 }
---
+++ tp/application/index/service/MoneyService.php
@@ -0,0 +1,34 @@
+<?php
+namespace app\index\service;
+
+use app\common\model\ModelFactory;
+
+class MoneyService
+{
+    public static function getInstance()
+    {
+        return new self();
+    }
+
+    /**
+     * @AUTHOR: Lamkakyun
+     * @DATE: 2018-06-09 12:52:35
+     * @DESCRIPTION: add a new money log
+     */
+    public function addMoneyLog($parmas)
+    {
+
+    }
+
+
+    /**
+     * add a new item
+     * @AUTHOR: Lamkakyun
+     * @DATE: 2018-06-09 12:54:54
+     * @DESCRIPTION:
+     */
+    public function addMoneyItem($params)
+    {
+
+    }
+}
