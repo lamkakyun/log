5596334c3b4a6a8a7619cd30bbc6faeb356ba987
lamkakyun <lamkakyun@gmail.com>
2018-06-10 14:36:02 +0800

    no comment

--- tp/application/index/controller/Task.php
+++ tp/application/index/controller/Task.php
@@ -1,18 +1,41 @@
 <?php
 namespace app\index\controller;
 
+use app\index\service\TaskService;
 use think\Controller;
 
 class Task extends Controller
 {
 
     /**
-     * add task
+     * task list
      * @AUTHOR: Lamkakyun
      * @DATE: 2018-05-31 10:46:58
      */
     public function index()
     {
+        $params = request()->request();
+        $task_list = TaskService::getInstance()->getTaskList($params);
+        return $task_list;
+    }
 
+
+    public function addTask()
+    {
+        $params = request()->request();
+        $ret = TaskService::getInstance()->editTask($params);
+    }
+
+    public function delTask()
+    {
+        $params = request()->request();
+        $ret = TaskService::getInstance()->delTask($params);
+    }
+
+
+    public function editTask()
+    {
+        $params = request()->request();
+        $ret = TaskService::getInstance()->editTask($params);
     }
 }
---
+++ tp/application/index/service/TaskService.php
@@ -0,0 +1,30 @@
+<?php
+namespace app\index\service;
+
+use app\common\model\ModelFactory;
+
+class TaskService
+{
+    public static function getInstance()
+    {
+        return new self();
+    }
+
+
+    public function getTaskList($params)
+    {
+
+    }
+
+
+    public function editTask($params)
+    {
+
+    }
+
+
+    public function delTask($params)
+    {
+
+    }
+}
